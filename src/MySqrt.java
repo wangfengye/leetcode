b3c2b7659dd78c0269b375a363630efa77423e72
wangfengye <1040441325@qq.com>
2018-09-25 14:24:19 +0800

    机器人返回原点

--- src/MySqrt.java
+++ src/MySqrt.java
@@ -3,32 +3,36 @@ public class MySqrt {
         if (x == 0) return 0;
         if (x < 4) return 1;
         int left = 0;
-        int right = x/2 ;
+        int right = x / 2;
         while (right - left > 0) {
             int mid = (left + right) / 2;
-            if (mid <= x/mid) {
-                left = mid+1;
-            }
-
-            else right = mid;
+            if (mid <= x / mid) {
+                left = mid + 1;
+            } else right = mid;
         }
-        return left-1;
+        return left - 1;
     }
 
     /**
-     * 牛顿法开方 做切线,无线逼近; x^2 =m;
-     * @param x
+     * 牛顿逼近法
+     * f(x) = x^2 - n
+     * 求 f(x) = 0 的解
+     * 取x0  做经过 (x0,f(x0))切线,交x轴于x1,重复操作 切线方程 f(x) =f(x0)+f'(xi)(x-xi);
+     * 可得公式  x(i+1)= (x(i)+n/x(i))/2
+     *
+     * @param x 平方数
      * @return
      */
     public static int mySqrt1(int x) {
-        if (x==0)return 0;
-        if (x<4)return 1;
-        int r = x/2;
-        while (r>x/r){
-            r=(r+x/r)/2;
+        if (x == 0) return 0;
+        if (x < 4) return 1;
+        int r = x / 2;
+        while (r > x / r) {
+            r = (r + x / r) / 2;
         }
         return r;
     }
+
     public static void main(String[] args) {
         System.out.println(MySqrt.mySqrt1(2147483647));
     }
