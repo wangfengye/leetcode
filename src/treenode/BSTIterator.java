850fc89f0b23186d59193ea8a00edb2ea00eec82
wangfengye <1040441325@qq.com>
2018-12-03 09:27:39 +0800

    二叉搜索树迭代器

---
+++ src/treenode/BSTIterator.java
@@ -0,0 +1,44 @@
+package treenode;
+
+import java.util.Stack;
+
+/**
+ * @author maple on 2018/12/3 9:07.
+ * @version v1.0
+ * @see 1040441325@qq.com
+ * 二叉搜索树迭代器
+ *
+ * 实现一个二叉搜索树迭代器。你将使用二叉搜索树的根节点初始化迭代器。
+ * 调用 next() 将返回二叉搜索树中的下一个最小的数。
+ * 注意: next() 和hasNext() 操作的时间复杂度是O(1)，并使用 O(h) 内存，其中 h 是树的高度
+ */
+public class BSTIterator {
+    Stack<TreeNode> tree = new Stack<>();
+    public BSTIterator(TreeNode root) {
+        while (root!=null){
+            tree.push(root);
+            root =root.left;
+        }
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !tree.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode result = tree.pop();
+        TreeNode current = result;
+        if(current.right != null){
+            current = current.right;
+            while(current != null){
+                tree.push(current);
+                current = current.left;
+            }
+        }
+        return result.val;
+    }
+
+
+}
---
+++ src/treenode/BSTIterator2.java
@@ -0,0 +1,50 @@
+package treenode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author maple on 2018/12/3 9:19.
+ * @version v1.0
+ * @see 1040441325@qq.com
+ */
+public class BSTIterator2 {
+    /**
+     * 中序遍历即为正确排序
+     */
+    ArrayList<Integer> list = new ArrayList<>();
+    private List<Integer> in(TreeNode root){
+        if (root.left!=null){
+            in(root.left);
+        }
+        list.add(root.val);
+        if (root.right!=null){
+            in(root.right);
+        }
+        return list;
+    }
+    public BSTIterator2(TreeNode root) {
+        in(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !list.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+       int val = list.get(0);
+       list.remove(0);
+       return val;
+    }
+
+    public static void main(String[] args){
+        TreeNode treeNode = new TreeNode(1);
+        TreeNode r1 = new TreeNode(2);
+        TreeNode r1l= new TreeNode(3);
+        treeNode.right =r1;
+        r1.left = r1l;
+
+    }
+}
