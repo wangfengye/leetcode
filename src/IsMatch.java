83382564707a40b1279230fb0b2196f145ab0e61
wangfengye <1040441325@qq.com>
2019-06-19 11:17:53 +0800

    leetcode

---
+++ src/IsMatch.java
@@ -0,0 +1,79 @@
+/**
+ * @author maple on 2019/6/19 9:27.
+ * @version v1.0
+ * @see 1040441325@qq.com
+ * 10. 正则表达式匹配
+ * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+ * <p>
+ * '.' 匹配任意单个字符
+ * '*' 匹配零个或多个前面的那一个元素
+ * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+ * <p>
+ * 说明:
+ * <p>
+ * s 可能为空，且只包含从 a-z 的小写字母。
+ * p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
+ * 示例 1:
+ * <p>
+ * 输入:
+ * s = "aa"
+ * p = "a"
+ * 输出: false
+ * 解释: "a" 无法匹配 "aa" 整个字符串。
+ * 示例 2:
+ * <p>
+ * 输入:
+ * s = "aa"
+ * p = "a*"
+ * 输出: true
+ * 解释: 因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+ * 示例 3:
+ * <p>
+ * 输入:
+ * s = "ab"
+ * p = ".*"
+ * 输出: true
+ * 解释: ".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+ * 示例 4:
+ * <p>
+ * 输入:
+ * s = "aab"
+ * p = "c*a*b"
+ * 输出: true
+ * 解释: 因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
+ * 示例 5:
+ * <p>
+ * 输入:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * 输出: false
+ */
+public class IsMatch {
+    public static boolean isMatch(String s, String p) {
+        boolean dp[][] = new boolean[s.length() + 1][p.length() + 1];
+        dp[s.length()][p.length()] = true;
+        for (int i = s.length(); i >= 0; i--) {
+            for (int j = p.length() - 1; j >= 0; j--) {
+                boolean first_match = (i < s.length() &&
+                        (p.charAt(j) == s.charAt(i) ||
+                                p.charAt(j) == '.'));
+                if (j + 1 < p.length() && p.charAt(j + 1) == '*') {//j+1位置为*时
+                    // 两种情况为true, 1:p增加了`字符*`, 2:s新增的字符匹配j开头的字符且,s未增加前符合规则,
+                    dp[i][j] = dp[i][j + 2] || first_match && dp[i + 1][j];
+                } else {//s,p新增的字符匹配,
+                    dp[i][j] = first_match && dp[i + 1][j + 1];
+                }
+            }
+        }
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[j][i]+" ");
+            }
+            System.out.println();
+        }
+        return dp[0][0];
+    }
+    public static void main(String[] a){
+        isMatch("aa","a*");
+    }
+}
