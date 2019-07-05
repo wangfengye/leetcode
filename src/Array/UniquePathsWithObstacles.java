f3fb8e966a162b80fa843bf8d090976a2197663a
wangfengye <1040441325@qq.com>
2019-07-05 09:50:52 +0800

    不同路径 II

---
+++ src/Array/UniquePathsWithObstacles.java
@@ -0,0 +1,66 @@
+package Array;
+
+/**
+ * @author maple on 2019/7/5 9:29.
+ * @version v1.0
+ * @see 1040441325@qq.com
+ * 63. 不同路径 II
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * <p>
+ * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+ * <p>
+ * <p>
+ * <p>
+ * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+ * <p>
+ * 说明：m 和 n 的值均不超过 100。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:
+ * [
+ *   [0,0,0],
+ *   [0,1,0],
+ *   [0,0,0]
+ * ]
+ * 输出: 2
+ * 解释:
+ * 3x3 网格的正中间有一个障碍物。
+ * 从左上角到右下角一共有 2 条不同的路径：
+ * 1. 向右 -> 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右 -> 向右
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/unique-paths-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class UniquePathsWithObstacles {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+        for (int i = 0; i < obstacleGrid[0].length; i++) {
+            if (obstacleGrid[0][i] == 1) break;
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < obstacleGrid.length; i++) {
+            if (obstacleGrid[i][0] == 0) dp[i][0] = dp[i - 1][0];
+            for (int j = 1; j < obstacleGrid[0].length; j++) {
+                if (obstacleGrid[i][j] == 0) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+
+    private void bfs(int[][] obstacleGrid, int i, int j) {
+        if (i == obstacleGrid.length - 1 && j == obstacleGrid[i].length - 1) {
+            res++;
+            return;
+        }
+        if (obstacleGrid[i][j] == 1) return;
+        if (i < obstacleGrid.length - 1) bfs(obstacleGrid, i + 1, j);
+        if (j < obstacleGrid[0].length - 1) bfs(obstacleGrid, i, j + 1);
+    }
+
+    int res;
+}
