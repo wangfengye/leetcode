91fdb062396934f2d690388584e0e588efe4d0e2
wangfengye <1040441325@qq.com>
2019-07-29 17:21:53 +0800

    另一颗树的子树

---
+++ src/treenode/IsSubtree.java
@@ -0,0 +1,56 @@
+package treenode;
+
+/**
+ * @author maple on 2019/7/29 17:11.
+ * @version v1.0
+ * @see 1040441325@qq.com
+ * 572. 另一个树的子树
+ * <p>
+ * 给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树。
+ * <p>
+ * 示例 1:
+ * 给定的树 s:
+ * <p>
+ * 3
+ * / \
+ * 4   5
+ * / \
+ * 1   2
+ * 给定的树 t：
+ * <p>
+ * 4
+ * / \
+ * 1   2
+ * 返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+ * <p>
+ * 示例 2:
+ * 给定的树 s：
+ * <p>
+ * 3
+ * / \
+ * 4   5
+ * / \
+ * 1   2
+ * /
+ * 0
+ * 给定的树 t：
+ * <p>
+ * 4
+ * / \
+ * 1   2
+ * 返回 false。
+ */
+public class IsSubtree {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (isSame(s, t)) return true;
+        if (s == null) return false;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        if (s.val != t.val) return false;
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+}
