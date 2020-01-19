af80a2d580759f416c78ad4d8344d64e880d1df6
wangfengye <1040441325@qq.com>
2020-01-19 10:11:15 +0800

    3题leetcode

---
+++ src/GetNoZeroIntegers.java
@@ -0,0 +1,59 @@
+/**
+ * 1317. 将整数转换为两个无零整数的和
+ * 无零整数」是十进制表示中 不含任何 0 的正整数。
+ *
+ * 给你一个整数 n，请你返回一个 由两个整数组成的列表 [A, B]，满足：
+ *
+ * A 和 B 都是无零整数
+ * A + B = n
+ * 题目数据保证至少有一个有效的解决方案。
+ *
+ * 如果存在多个有效解决方案，你可以返回其中任意一个。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：n = 2
+ * 输出：[1,1]
+ * 解释：A = 1, B = 1. A + B = n 并且 A 和 B 的十进制表示形式都不包含任何 0 。
+ * 示例 2：
+ *
+ * 输入：n = 11
+ * 输出：[2,9]
+ * 示例 3：
+ *
+ * 输入：n = 10000
+ * 输出：[1,9999]
+ * 示例 4：
+ *
+ * 输入：n = 69
+ * 输出：[1,68]
+ * 示例 5：
+ *
+ * 输入：n = 1010
+ * 输出：[11,999]
+ */
+public class GetNoZeroIntegers {
+    public int[] getNoZeroIntegers(int n) {
+        int res[]=new int[2];
+        for (int i = 1; i < n; i++) {
+            if (containZero(i))continue;
+            int b=n-i;
+            if(!containZero(b)){
+                res[0]=i;
+                res[1]=b;
+                return res;
+            }
+        }
+        return res;
+    }
+    private boolean containZero(int n){
+        if(n==0)return true;
+        while (n>0){
+            if(n%10==0)return true;
+            n/=10;
+        }
+        return false;
+    }
+}
