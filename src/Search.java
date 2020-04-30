678bc50f36ef632e915959cd0cca02f7e8389427
wangfengye <1040441325@qq.com>
2020-04-30 10:05:37 +0800

    快乐数

--- src/Search.java
+++ src/Search.java
@@ -28,26 +28,5 @@ public class Search {
         }
         return -1;
     }
-    public int search1(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] == target) return mid;
-            if (nums[mid] > nums[right]) {
-                if (nums[mid] < target || target<= nums[right]) {
-                    left = mid +1;
-                } else {//不在左侧,
-                    right = mid-1;
-                }
-            } else {//右侧有序
-                if (nums[mid]<target && target<= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
-        }
-        return -1;
-    }
+
 }
